0eca0e3a9c9fa5c379e846255dcca5af422c0830
Phelipe Melo Matos <70668772+phelipee15@users.noreply.github.com>
2023-10-31 00:22:13 -0300

    Add files via upload

---
+++ System Skin Panel/global.lua
@@ -0,0 +1,45 @@
+config = {
+	["menu"] = {
+		["bind"] = "F4",
+	},
+	["skins"] = { -- ID, Nome
+		{0, "CJ"},
+		{2, "Skin 2"},
+		{3, "Skin 3"},
+		{4, "Skin 4"},
+		{5, "Skin 5"},
+		{6, "Skin 6"},
+		{7, "Skin 7"},
+		{8, "Skin 8"},
+		{9, "Skin 9"},
+		{10, "Skin 10"},
+		{11, "Skin 11"},
+		{12, "Skin 12"},
+		{13, "Skin 13"},
+		{14, "Skin 14"},
+		{15, "Skin 15"},
+	},
+}
+
+lang = {
+	["portuguese"] = {
+		["title"] = "Painel Skin",
+		["category"] = {"ID", "Nome"},
+		["button_select"] = "Pegar",
+		["button_close"] = "Fechar",
+		["notify"] = {
+			["errorID"] = "Skin ID inválido!",
+			["errorSelect"] = "Skin não selecionada!",
+		},
+	},
+	["english"] = {
+		["title"] = "Panel Skin",
+		["category"] = {"ID", "Name"},
+		["button_select"] = "Get",
+		["button_close"] = "Close",
+		["notify"] = {
+			["errorID"] = "Invalid Skin ID!",
+			["errorSelect"] = "Skin not selected!",
+		},
+	},
+}
